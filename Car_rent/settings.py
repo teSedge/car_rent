050302a77733af11967a1c46c3cff824130a3c16
teSedge <teSedge99@gmail.com>
2022-06-14 21:07:04 +0300

    0.8

--- Car_rent/settings.py
+++ Car_rent/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a3km-o8-svk*x6wt*!2m04$)5@*3mqisw0$_jnlz+p_&lel7xl'
+
+# SECRET_KEY = 'django-insecure-a3km-o8-svk*x6wt*!2m04$)5@*3mqisw0$_jnlz+p_&lel7xl'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
@@ -137,3 +139,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
